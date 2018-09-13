2bf0b1dfdb730e5153eef67926638623be8df650
Stephen French <stephenfrench9@gmail.com>
2018-09-12 18:40:48 -0700

    image is loaded to the search fragment. Images are only loaded once, and the user never sees their own image

--- database/app/src/main/java/com/google/firebase/quickstart/database/fragment/Advise.java
+++ database/app/src/main/java/com/google/firebase/quickstart/database/fragment/Advise.java
@@ -24,12 +24,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.quickstart.database.R;
+import com.google.firebase.quickstart.database.models.Post;
 import com.google.firebase.quickstart.database.utilities.Util;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+
+import static com.google.firebase.quickstart.database.utilities.Util.downloadImage;
+import static com.google.firebase.quickstart.database.utilities.Util.getUid;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -70,19 +75,47 @@ public class Advise extends Fragment {
                 Iterable<DataSnapshot> van = dataSnapshot.getChildren();
                 String person = "";
                 for(DataSnapshot user : van) {
-                    if(Util.getUid().equals(user.getKey())) {
+                    if(getUid().equals(user.getKey())) {
                         person = person + ", " + user.getKey();
                         Log.d("CHECKPOINT", user.getKey());
                     }
                     Log.d("CHECKPOINT", user.getKey());
-                    Log.d("CHECKPOINT", Util.getUid());
-                    Log.d("CHECKPOINT", String.valueOf(Util.getUid().equals(user.getKey())));
+                    Log.d("CHECKPOINT", getUid());
+                    Log.d("CHECKPOINT", String.valueOf(getUid().equals(user.getKey())));
                 }
 
 
-                mTextView.setText(person + ", " + Util.getUid());
+//                mTextView.setText(person + ", " + getUid());
             }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        };
+
+        ValueEventListener m = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> van = dataSnapshot.getChildren();
+                String one = "";
+                String postKey = "";
+                for(DataSnapshot post : van) { //a for loop over one thing.
+                    postKey = post.getKey().toString();
+                    mDb.child("posts").child(postKey).child(getUid()).setValue(333);
+                    one = (String) post.child("addressOne").getValue();
+                }
+
+                String addressS = "";
+                for(String location : one.split("/")) {
+                    addressS = addressS + "\n" + location;
+                }
+                Log.d("CHECKPOINT", "onCreateView(), onDataChange(): " + addressS);
+                mTextView.setText(addressS);
+                String[] address = one.split("/");
+                StorageReference node = mStorage.child(address[0]).child(address[1]).child(address[2]);
+                downloadImage(node, mImageView);
+
+            }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
@@ -90,6 +123,11 @@ public class Advise extends Fragment {
         };
 
         node.addValueEventListener(l);
+
+        Query aNode = mDb.child("posts").orderByChild(getUid()).equalTo(null).limitToFirst(1);
+        aNode.addListenerForSingleValueEvent(m);
+
+
         //iterate over the children
         //
         //call getKey on each of the children, add to a collection

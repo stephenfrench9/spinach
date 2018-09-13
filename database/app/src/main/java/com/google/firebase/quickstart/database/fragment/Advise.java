5264df9d2ce8d9ee08198600f482110dba125071
Stephen French <stephenfrench9@gmail.com>
2018-09-12 21:27:25 -0700

    clean up advise fragment

--- database/app/src/main/java/com/google/firebase/quickstart/database/fragment/Advise.java
+++ database/app/src/main/java/com/google/firebase/quickstart/database/fragment/Advise.java
@@ -36,39 +36,23 @@ import java.util.Iterator;
 import static com.google.firebase.quickstart.database.utilities.Util.downloadImage;
 import static com.google.firebase.quickstart.database.utilities.Util.getUid;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Advise extends Fragment {
 
-    public  Button mButton;
-    private Bitmap mResult;
     private DatabaseReference mDb;
     private ImageView mImageView;
     private StorageReference mStorage;
-    private StorageReference mNode;
-    private String mNodePath;
     public  TextView mTextView;
-    private File mLocalFile;
-
-
 
 
     public Advise() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.advise, container, false);
-        Log.d("ADVISE", "the logger is working");
         mDb = FirebaseDatabase.getInstance().getReference();
-
         DatabaseReference node = mDb.child("user-posts");
-
-        //read the node -> I get a datasnapshot, which has many children.
         ValueEventListener l = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -92,7 +76,6 @@ public class Advise extends Fragment {
 
             }
         };
-
         ValueEventListener m = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -112,7 +95,7 @@ public class Advise extends Fragment {
                 Log.d("CHECKPOINT", "onCreateView(), onDataChange(): " + addressS);
                 mTextView.setText(addressS);
                 String[] address = one.split("/");
-                
+
                 if(!one.equals("")) {
                     StorageReference node = mStorage.child(address[0]).child(address[1]).child(address[2]);
                     downloadImage(node, mImageView);
@@ -124,59 +107,18 @@ public class Advise extends Fragment {
 
             }
         };
-
         node.addValueEventListener(l);
-
         Query aNode = mDb.child("posts").orderByChild(getUid()).equalTo(null).limitToFirst(1);
         aNode.addListenerForSingleValueEvent(m);
-
-
-        //iterate over the children
-        //
-        //call getKey on each of the children, add to a collection
-        //
-        //display the collection in the main screen.
-
         mImageView = rootView.findViewById(R.id.advise_picture);
         mTextView = rootView.findViewById(R.id.intro);
         mStorage = FirebaseStorage.getInstance().getReference();
-        mNode = mStorage.child("s1feNUUH08grUcZTtZvRqzstOvm2").child("-LLrvsPm9y2Zkke60-lr").child("one");
-        mNodePath = mNode.getPath();
-
-//        mButton.setVisibility(View.INVISIBLE);
-
-        ValueEventListener listen = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                mResult = dataSnapshot.getValue(Bitmap.class);
-                mImageView.setImageBitmap(mResult);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-
-        mLocalFile = null;
-        try {
-            mLocalFile = File.createTempFile("images", "jpg");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        Util.downloadImage(mNode, mImageView);
-
-        // Inflate the layout for this fragment
         return rootView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-
     }
 }
 

3e5a3eb2da06748965eab19facc04349e3d1040a
Stephen French <stephenfrench9@gmail.com>
2018-10-02 11:31:29 -0700

    the sign IN intent now returns to onActivityRestult

--- database/app/src/main/java/com/google/firebase/quickstart/database/FirebaseUIActivity.java
+++ database/app/src/main/java/com/google/firebase/quickstart/database/FirebaseUIActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.quickstart.database.models.User;
 
 import java.util.Collections;
 
@@ -45,25 +47,32 @@ public class FirebaseUIActivity extends AppCompatActivity implements View.OnClic
         ActionBar greg = getSupportActionBar();
         greg.hide();
 
+        Log.d("BASIL", "onCreate(): end");
+
     }
 
     @Override
     protected void onStart() {
+        Log.d("BASIL", "onStart(): start");
         super.onStart();
         updateUI(mAuth.getCurrentUser());
-        if(mAuth.getCurrentUser()!=null) {
-            startActivity(new Intent(FirebaseUIActivity.this, MainActivity.class));
-        }
+        Log.d("BASIL", "onStart(): end");
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.d("BASIL", "onActivityResult(): start");
+
 
         if (requestCode == RC_SIGN_IN) {
+            Log.d("BASIL", "onActivityResult(): middle");
             if (resultCode == RESULT_OK) {
                 // Sign in succeeded
-                updateUI(mAuth.getCurrentUser());
+                FirebaseUser user = mAuth.getCurrentUser();
+                boolean verified = user.isEmailVerified();
+                Log.d("BASIL", "onActivityResult(): " + String.valueOf(verified));
+                updateUI(user);
                 startActivity(new Intent(FirebaseUIActivity.this, MainActivity.class));
                 finish();
             } else {
@@ -72,11 +81,13 @@ public class FirebaseUIActivity extends AppCompatActivity implements View.OnClic
                 updateUI(null);
             }
         }
+        Log.d("BASIL", "onActivityResult(): end");
     }
 
     private void startSignIn() {
         // Build FirebaseUI sign in intent. For documentation on this operation and all
         // possible customization see: https://github.com/firebase/firebaseui-android
+        Log.d("BASIL", "StartSignIn(): start");
         Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
                 .setIsSmartLockEnabled(!BuildConfig.DEBUG)
                 .setAvailableProviders(Collections.singletonList(
@@ -84,10 +95,13 @@ public class FirebaseUIActivity extends AppCompatActivity implements View.OnClic
                 .setLogo(R.mipmap.ic_launcher)
                 .build();
 
+        Log.d("BASIL", "StartSignIn(): middle");
         startActivityForResult(intent, RC_SIGN_IN);
+        Log.d("BASIL", "StartSignIn(): end");
     }
 
     private void updateUI(FirebaseUser user) {
+        Log.d("BASIL", "updateUI(): start");
         if (user != null) {
             // Signed in
             mStatusView.setText(getString(R.string.firebaseui_status_fmt, user.getEmail()));
@@ -103,11 +117,14 @@ public class FirebaseUIActivity extends AppCompatActivity implements View.OnClic
             findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
             findViewById(R.id.sign_out_button).setVisibility(View.GONE);
         }
+        Log.d("BASIL", "updateUI(): end");
     }
 
     private void signOut() {
+        Log.d("BASIL", "singOut(): start");
         AuthUI.getInstance().signOut(this);
         updateUI(null);
+        Log.d("BASIL", "singOut(): end");
     }
 
     @Override
